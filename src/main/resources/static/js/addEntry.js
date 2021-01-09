e498baebb3783b4bceb7bb2a0795120f885b0119
ksamoviski <ksamoviski@gmail.com>
2021-01-09 10:50:18 -0500

    playing with addEntry js

--- src/main/resources/static/js/addEntry.js
+++ src/main/resources/static/js/addEntry.js
@@ -1,18 +1,56 @@
-const testing = document.getElementById("testing");
-testing.innerText="testing java";
+const profilePetDropDownButton = document.getElementById('pet__dropdown');
+const reasonChoice = document.getElementById('reasons__dropdown');
+const hypoAllergenic = document.getElementById('Hypoallergenic');
+const goodWithKids = document.getElementById('Good-with-kids');
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+profilePetDropDownButton.addEventListener("click", function () {
+    if (profilePetDropDownButton.selectedIndex === 1){
+        profilePetDropDownButton.value = "Dog";
+    } else if (profilePetDropDownButton.selectedIndex === 2) {
+        profilePetDropDownButton.value = "Cat";
+    } else if (profilePetDropDownButton.selectedIndex === 3) {
+        profilePetDropDownButton.value = "Rabbit";
+    } else if (profilePetDropDownButton.selectedIndex === 4) {
+        profilePetDropDownButton.value = "Mini Horse";
+    } else if (profilePetDropDownButton.selectedIndex === 5) {
+        profilePetDropDownButton.value = "Lizard";
+    }
+});
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
+reasonChoice.addEventListener("click", function () {
+    if (reasonChoice.selectedIndex === 1){
+        reasonChoice.value = "For Play";
+    } else if (reasonChoice.selectedIndex === 2) {
+        reasonChoice.value = "For Companionship";
+    } else if (reasonChoice.selectedIndex === 3) {
+        reasonChoice.value = "For Therapy";
+    }
+});
+
+hypoAllergenic.addEventListener("click", function (){
+    hypoAllergenic.value = "true";
+})
+
+goodWithKids.addEventListener("click", function (){
+    goodWithKids.value = "true";
+})
+
+
+
+
+
+
+
+$(function () {
+    $("#firstName, #lastName").bind("change keyup",
+        function () {
+            if ($("#firstName").val() && $("#lastName").val()
+                && $("#age").val() && $("#email").val() && $("#location").val()
+                && $("#reasons__dropdown").val() !== "")
+
+                $(this).closest("form").find(":submit").removeAttr("disabled");
+            else
+                $(this).closest("form").find(":submit").attr("disabled", "disabled");
+        });
+});
