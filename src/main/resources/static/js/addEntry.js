b5bb4c0e5938ef3f805cc5a1e20a764972b2ea0d
kfiddle <kenjfiddle@gmail.com>
2021-01-10 14:15:59 -0500

    simple dropdown

--- src/main/resources/static/js/addEntry.js
+++ src/main/resources/static/js/addEntry.js
@@ -1,3 +1,12 @@
+const seasonDropdown = document.getElementById('seasonDropdown');
+const habitatDropdown = document.getElementById('habitatDropdown');
+
+const dropDownHeader = document.querySelector('.dropDownHeader');
 
 
+
+seasonDropdown.addEventListener('click', ()=> {
+
+
+})
 
