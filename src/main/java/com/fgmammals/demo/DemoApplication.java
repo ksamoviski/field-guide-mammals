d8e0a2d4bd62de166c3503b17564d8cff38026cc
kfiddle <kenjfiddle@gmail.com>
2021-02-01 16:20:14 -0500

    more for heroku

--- src/main/java/com/fgmammals/demo/DemoApplication.java
+++ src/main/java/com/fgmammals/demo/DemoApplication.java
@@ -3,11 +3,12 @@ package com.fgmammals.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
