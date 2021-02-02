e3f8762602a54dd26a0a09927d5e88e01686f302
kfiddle <kenjfiddle@gmail.com>
2021-02-02 18:42:03 -0500

    finally, dropdowns behave

--- src/main/resources/static/js/addEntry.js
+++ src/main/resources/static/js/addEntry.js
@@ -5,80 +5,71 @@ const habitatButton = document.getElementById("habitatButton");
 const habitatDropdown = document.getElementById("habitatDropdown");
 
 
-let seasonClicked = false;
-let habitatClicked = false;
+let seasonsClicked = false;
+let habitatsClicked = false;
 
-const habitats = ["Wetlands", "Forest", "My Backyard", "My Driveway", "Archer's Backyard", "The Park"];
 const seasons = ["Winter", "Spring", "Summer", "Autumn"];
+const habitats = ["Wetlands", "Forest", "My Backyard", "My Driveway", "Archer's Backyard", "The Park"];
+
+// const menus = [seasons, habitats];
+
+
+const removeMenu = (menu) => {
+    if (menu === "seasons") {
+        for (let items = 0; items < seasons.length; items++) {
+            seasonsDropdown.removeChild(seasonsDropdown.lastChild);
+        }
+        seasonsClicked = false;
+    } else {
+        for (let items = 0; items < habitats.length; items++) {
+            habitatDropdown.removeChild(habitatDropdown.lastChild);
+        }
+        habitatsClicked = false;
+    }
+};
+
+const addMenu = (menu) => {
+    if (menu === "seasons") {
+        seasons.forEach(season => {
+            let item = document.createElement('p');
+            item.className = "dropdownItem";
+            item.innerText = season;
+            seasonsDropdown.appendChild(item);
+        });
+        seasonsClicked = true;
+    } else {
+        habitats.forEach(habitat => {
+            let item = document.createElement('p');
+            item.className = "dropdownItem";
+            item.innerText = habitat;
+            habitatDropdown.appendChild(item);
+        });
+        habitatsClicked = true;
+    }
+};
 
 seasonButton.addEventListener('click', () => {
-        if (seasonClicked === false) {
-            seasons.forEach(season => {
-                let item = document.createElement('p');
-                item.className = "dropdownItem";
-                item.innerText = season;
-                seasonsDropdown.appendChild(item);
-            })
-            seasonClicked = true;
-        } else {
-            for (let i = 0; i < seasons.length; i++) {
-                seasonsDropdown.removeChild(seasonsDropdown.lastChild);
-            }
-            seasonClicked = false;
-        }
+    if (!seasonsClicked && !habitatsClicked){
+        addMenu("seasons");
+    } else if (!seasonsClicked && habitatsClicked) {
+        addMenu("seasons");
+        removeMenu("habitats");
+    } else if (seasonsClicked === true) {
+        removeMenu("seasons");
     }
-)
+});
 
 habitatButton.addEventListener('click', () => {
-        if (habitatClicked === false) {
-            habitats.forEach(habitat => {
-                let itemClicked = false;
-
-                let item = document.createElement('p');
-                item.className = "dropdownItem";
-                item.innerText = habitat;
-                habitatDropdown.appendChild(item);
-
-                item.addEventListener('mouseover', () => {
-                    // item.classList.add("habitatItemHover");
-
-                    item.style.color = "darkred";
-                });
-
-                item.addEventListener('mouseout', () => {
-                    item.classList.remove("habitatItemHover");
-
-                })
-                item.addEventListener('click', () => {
-                    if (itemClicked === false) {
-                        // item.style.color = "chartreuse";
-                        // item.style.fontSize = "140%";
-
-                        setTimeout(finalItemShow, 300, item);
-                        item.classList.add('shownItem');
-                        itemClicked = true;
-
-                    } else {
-                        // item.style.color = "#ccc";
-                        itemClicked = false;
-                    }
-                });
-            });
-            habitatClicked = true;
-        } else {
-            for (let i = 0; i < habitats.length; i++) {
-                habitatDropdown.removeChild(habitatDropdown.lastChild);
-            }
-            habitatClicked = false;
-        }
+    if (!habitatsClicked && !seasonsClicked) {
+        addMenu("habitats");
+    } else if (!habitatsClicked && seasonsClicked) {
+        addMenu("habitats");
+        removeMenu("seasons");
+    } else if (habitatsClicked === true) {
+        removeMenu("habitats");
     }
-);
+});
 
-function finalItemShow(item) {
-    item.classList.remove('shownItem');
-    // item.style.fontSize = "100%";
-    item.style.color = "#808622";
-};
 
 
 
